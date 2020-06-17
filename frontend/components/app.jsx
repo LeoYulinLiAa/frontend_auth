be768548e72e77844086cf31399b52df21d486ad
LeoYulinLi <LeoYulinLi@gmail.com>
2020-06-17 10:52:18 -0700

    Phase 6 done

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -4,11 +4,13 @@ import NavBarContainer from './nav_bar/nav_bar_container';
 import ChirpIndexContainer from './chirps/chirp_index_container';
 import Home from './home/home';
 import { Route } from 'react-router-dom';
+import SignUp from "./session/signup";
 
 export default () => (
   <div>
     <Route path="/" component={NavBarContainer} />
     <Route exact path="/" component={Home} />
     <Route path="/chirps" component={ChirpIndexContainer} />
+    <Route path="/signup" component={SignUp} />
   </div>
 );
--- frontend/components/session/signup.jsx
+++ frontend/components/session/signup.jsx
@@ -1,6 +1,36 @@
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { createNewUser } from "../../actions/session";
 
 const SignUp = props => {
 
+  const dispatch = useDispatch();
+
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    dispatch(createNewUser({ username, email, password}))
+      .then(() => props.history.push("/chirps"));
+  }
+
+  return <div className="session-form">
+    <h2>Sign up</h2>
+    <form onSubmit={handleSubmit}>
+      <label>Username:
+        <input type="text" value={username} onChange={event => setUsername(event.target.value)} />
+      </label>
+      <label>Email:
+        <input type="email" value={email} onChange={event => setEmail(event.target.value)} />
+      </label>
+      <label>Password:
+        <input type="password" value={password} onChange={event => setPassword(event.target.value)} />
+      </label>
+      <button>Sign up</button>
+    </form>
+  </div>
 }
 
-export default SignUp
+export default SignUp;
